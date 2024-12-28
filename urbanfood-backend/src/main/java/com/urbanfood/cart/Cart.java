dfe9c9ed6642f29737c6620f3abd42c1912cd80b
madhushan <surangamadhushan24@gmail.com>
2024-12-28 18:25:16 +0530

    add product cart and react

--- urbanfood-backend/src/main/java/com/urbanfood/cart/Cart.java
+++ urbanfood-backend/src/main/java/com/urbanfood/cart/Cart.java
@@ -1,6 +1,22 @@
 package com.urbanfood.cart;
 
+import com.urbanfood.product.Product;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "cart")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cart {
     private String id;
+    private Double totalAmount;
+    private List<Product> products;
 
 }
--- urbanfood-backend/src/main/java/com/urbanfood/product/Product.java
+++ urbanfood-backend/src/main/java/com/urbanfood/product/Product.java
@@ -4,18 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Document(collection = "products")
 public class Product {
 
+    @Id
     private String id;
     private String name;
     private String description;
     private String category;
     private Double price;
+    private String supplierId;
     private Integer stock;
 
 
